43be34a8b5fc4931b32567834020b065e8d4483d
krisdelosreyes <kdelosreyes@deloitte.com>
2021-10-28 18:46:25 +0800

    Added Multifield component

---
+++ aemtesting-core/src/main/java/com/deloitte/components/NavigationList.java
@@ -0,0 +1,24 @@
+package com.deloitte.components;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class NavigationList {
+
+    @Inject
+    private String[] navigationEntry;
+
+    public List<String> getNavigationList() {
+        return Arrays.asList(navigationEntry);
+    }
+
+    private List<String> navigationList;
+}
